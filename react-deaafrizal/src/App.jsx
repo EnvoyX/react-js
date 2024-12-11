874fdadad8f328f8a93f2b8749bf4ddcc55f076e
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-11 09:20:29 +0700

    learned about Components in react js

--- react-deaafrizal/src/App.jsx
+++ react-deaafrizal/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import MyButton from "./components/myButton";
+import { Navbar } from "./components/MyNavbar";
+import Footer from "./components/Footer";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <Navbar></Navbar>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,19 +21,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Halo Cuyyy</p>
+      <MyButton></MyButton>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer></Footer>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ react-deaafrizal/src/components/MyNavbar.jsx
@@ -0,0 +1,17 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
