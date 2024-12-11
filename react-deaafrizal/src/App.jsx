9d9a801468dea5fe4ba4fa5b7a3b9308e3f9e55d
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-11 12:47:10 +0700

    learned useEffect in react js

--- react-deaafrizal/src/App.jsx
+++ react-deaafrizal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import MyButton from "./components/myButton";
@@ -25,10 +25,50 @@ const App = () => {
   };
 
   // useState
+  // Kiri ==> Getter (Initial Value, Mutable Value)
+  // Kanan ==> Setter (Function that changes value)
   const [getNavbarValue, setNavbarValue] = useState("");
   const handleNavbarValue = () => {
     setNavbarValue("My Contact");
   };
+
+  const [getGirlfriendValue, setGirlfriendValue] = useState(1);
+  const [getGirlfriendName, setGirlfriendName] = useState("");
+
+  const incrementGirlfriend = () => {
+    setGirlfriendValue((prev) => prev + 1);
+  };
+  const decrementGirlfriend = () => {
+    if (getGirlfriendValue <= 0) return;
+    setGirlfriendValue((prev) => prev - 1);
+  };
+
+  // if there's a change of data, states whatever in functional Component App or any Component,
+  //useEffect will autimatically executed
+  //
+  useEffect(() => {
+    console.log("testing");
+  });
+
+  // this useEffect will Render autimatically first-time after load the web
+  // but will not trigger if there's any changes in data or states
+  useEffect(() => {
+    alert("Hello hehehe");
+  }, []);
+
+  useEffect(() => {
+    setGirlfriendName("Herta");
+  }, []);
+
+  // if there's any changes in getGirlfriendValue,
+  // will trigger useEffect
+  useEffect(() => {
+    if (getGirlfriendValue > 1) return setGirlfriendName("HERTAS");
+    else if (getGirlfriendValue === 1) return setGirlfriendName("The HERTA");
+    else if (getGirlfriendValue === 0)
+      return setGirlfriendName("No Girlfriend");
+    alert("Girlfriend Herta quantity changed!");
+  }, [getGirlfriendValue]);
 
   return (
     <>
@@ -47,8 +87,19 @@ const App = () => {
       </div>
       <h1>Vite + React</h1>
       <p>Halo Cuyyy</p>
-      <MyButton clicked={clicked}></MyButton>
-      <button onClick={() => handleNavbarValue()}>Ubah Navigasi</button>
+      <h5>Girlfriend name: {getGirlfriendName}</h5>
+      <p>Girlfriends: {getGirlfriendValue}</p>
+      <div className="list-buttons">
+        <MyButton clicked={clicked}></MyButton>
+        <button onClick={() => handleNavbarValue()}>Ubah Navigasi</button>
+        {/* callback bawaan untuk function pengubah state di useState  */}
+        {/* dimana prev sebagai latest data dari state */}
+        {/* Sebagai best practice */}
+        <button onClick={() => incrementGirlfriend()}>
+          Add more girlfriend
+        </button>
+        <button onClick={() => decrementGirlfriend()}>remove girlfriend</button>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
