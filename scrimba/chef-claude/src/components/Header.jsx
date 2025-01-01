60cba6a2359011989c76e86280d8ee58d62a3036
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-01-01 21:46:20 +0700

    Chef-Claude Update 1

---
+++ scrimba/chef-claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefIcon from '../images/chef-claude-icon.png';
+
+export const Header = () => {
+  return (
+    <header>
+      <img src={chefIcon} alt="Chef Claude Icon" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
---
+++ scrimba/chef-claude/src/components/Main.jsx
@@ -0,0 +1,14 @@
+export const Main = () => {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. garlic"
+          aria-label="Add ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+    </main>
+  );
+};
